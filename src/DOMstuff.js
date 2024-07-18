f432edd3a5cee9ba343ecd5447c8f842cda48d30
Vecchi <79600745+VecchiR@users.noreply.github.com>
2024-07-18 10:13:53 -0300

    Start refactoring "DisplayController"*
    *From tic-tac-toe project
    
    For now, it's possible to render the player and opponent's boards
    (the player board shows the position of the ships, while the opponent's)
    only shows the attacked coordinates ('hit' or 'miss')

--- src/DOMstuff.js
+++ src/DOMstuff.js
@@ -1,30 +1,140 @@
-export function initializeGrid(gridContainer) {
+import { Ship, GameBoard, Player, GameFlow } from './classes.js';
 
-    for (let i = 0; i < 100; i++) {
-        const sqrDiv = document.createElement('div');
-        sqrDiv.className = 'cell';
-        gridContainer.appendChild(sqrDiv);
 
-        // sqrDiv.addEventListener('mouseenter', () => {
-        //     if (randomColorMode) {
-        //         let randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-        //         sqrDiv.style.backgroundColor = randomColor;
-        //     }
+export class DisplayController {
+    constructor() {
+        this.displayBoard = null;
+        this.msgDisplay = null;
+        this.resetButton = null;
+        this.gameContainer = null;
 
-        //     else {
-        //         if (progressiveDarkeningMode === true && !sqrDiv.style.backgroundColor) {
-        //             sqrDiv.style.backgroundColor = "#909090";
-        //         }
+        this.playerXName = document.querySelector('#playerx');
+        this.playerOName = document.querySelector('#playero');
 
-        //         else if (progressiveDarkeningMode === true && sqrDiv.style.backgroundColor) {
-        //             let newColor = updateColor(sqrDiv.style.backgroundColor);
-        //             sqrDiv.style.backgroundColor = newColor;
-        //         }
+        this.startContainer = document.querySelector('.start-container');
+        this.mainContainer = document.querySelector('.main-container');
 
-        //         else {
-        //             sqrDiv.style.backgroundColor = 'black';
-        //         }
-        //     }
-        // })
+        this.leftBoard = document.querySelector('.player-board');
+        this.rightBoard = document.querySelector('.opp-board');
     }
+
+    removeStartScreen() {
+        gameflow.setPlayers(
+            this.playerXName.value ? this.playerXName.value : this.playerXName.placeholder,
+            this.playerOName.value ? this.playerOName.value : this.playerOName.placeholder
+        );
+        this.mainContainer.removeChild(this.startContainer);
+        return true;
+    }
+
+    resetScreen() {
+        gameboard.resetBoard();
+        gameflow.turnOffGameOverFlag();
+        this.mainContainer.removeChild(this.gameContainer);
+        this.mainContainer.appendChild(this.startContainer);
+    }
+
+    createBoard() {
+        this.gameContainer = document.createElement('div');
+        this.gameContainer.setAttribute('class', 'game-container');
+        this.msgDisplay = document.createElement('span');
+        this.msgDisplay.setAttribute('class', 'messages');
+        this.resetButton = document.createElement('button');
+        this.resetButton.setAttribute('class', 'reset');
+        this.resetButton.innerHTML = 'Reset';
+        this.resetButton.addEventListener('click', () => this.resetScreen());
+        const board = document.createElement('div');
+        board.setAttribute('class', 'board');
+        for (let x = 0; x < 3; x++) {
+            for (let y = 0; y < 3; y++) {
+                let space = document.createElement('div');
+                space.setAttribute('class', 'space');
+                space.setAttribute('spcx', x);
+                space.setAttribute('spcy', y);
+                space.addEventListener('click', () => gameflow.selectSpace(space.getAttribute('spcx'), space.getAttribute('spcy')));
+                board.appendChild(space);
+            }
+        }
+        this.gameContainer.appendChild(this.msgDisplay);
+        this.gameContainer.appendChild(board);
+        this.gameContainer.appendChild(this.resetButton);
+        this.mainContainer.appendChild(this.gameContainer);
+        this.displayBoard = document.querySelectorAll('.space');
+        this.updMsgDisplay('turn');
+    }
+
+    updMsgDisplay(arg) {
+        if (arg === 'turn') {
+            this.msgDisplay.innerHTML = `${gameflow.getActivePlayer().name}, it's your turn!`;
+        } else {
+            this.msgDisplay.innerHTML = arg;
+        }
+    }
+
+    updDisplayBoard() {
+        this.displayBoard.forEach(space => {
+            space.textContent = gameboard.getBoard()[space.getAttribute('spcx')][space.getAttribute('spcy')];
+        });
+    }
+
+    initializeGrid(gridContainer) {
+
+        for (let i = 0; i < 100; i++) {
+            const sqrDiv = document.createElement('div');
+            sqrDiv.className = 'cell';
+            gridContainer.appendChild(sqrDiv);
+
+            // sqrDiv.addEventListener('mouseenter', () => {
+            //     if (randomColorMode) {
+            //         let randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+            //         sqrDiv.style.backgroundColor = randomColor;
+            //     }
+
+            //     else {
+            //         if (progressiveDarkeningMode === true && !sqrDiv.style.backgroundColor) {
+            //             sqrDiv.style.backgroundColor = "#909090";
+            //         }
+
+            //         else if (progressiveDarkeningMode === true && sqrDiv.style.backgroundColor) {
+            //             let newColor = updateColor(sqrDiv.style.backgroundColor);
+            //             sqrDiv.style.backgroundColor = newColor;
+            //         }
+
+            //         else {
+            //             sqrDiv.style.backgroundColor = 'black';
+            //         }
+            //     }
+            // })
+        }
+    }
+
+    renderPlayerBoard(player) {
+        const board = player.playerBoard.board;
+
+        // Loop through each row of the board
+        board.forEach((row, rowIndex) => {
+            // Loop through each cell in the row
+            row.forEach((cellContent, cellIndex) => {
+                const cell = this.leftBoard.children[rowIndex * 10 + cellIndex]; // Calculate cell index
+                cell.textContent = cellContent; // Set the cell content
+            });
+        });
+    }
+
+    renderOpponentBoard(player) {
+        const board = player.playerBoard.board;
+
+        // Loop through each row of the board
+        board.forEach((row, rowIndex) => {
+            // Loop through each cell in the row
+            row.forEach((cellContent, cellIndex) => {
+                if (cellContent === '' || cellContent === 'hit' || cellContent === 'miss') {
+                    const cell = this.rightBoard.children[rowIndex * 10 + cellIndex]; // Calculate cell index
+                    cell.textContent = cellContent; // Set the cell content
+                }
+            });
+        });
+    }
+
+
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
-import {Ship, GameBoard, Player} from './classes.js';
-import {createGrid, initializeGrid} from './DOMstuff.js';
+import { Ship, GameBoard, Player, GameFlow } from './classes.js';
+import { DisplayController } from './DOMstuff.js';
 import './style.css';
 
+const display = new DisplayController();
+const flow = new GameFlow();
+flow.player1.playerBoard.placeShip(flow.player1.playerBoard.ships.battleship, [1, 1], false);
+flow.player1.playerBoard.placeShip(flow.player1.playerBoard.ships.carrier, [8, 4], false);
+flow.player1.playerBoard.placeShip(flow.player1.playerBoard.ships.destroyer, [2, 10], true);
+flow.player1.playerBoard.placeShip(flow.player1.playerBoard.ships.patrol, [9, 9], false);
+flow.player1.playerBoard.placeShip(flow.player1.playerBoard.ships.submarine, [3, 3], false);
 
-initializeGrid(document.querySelector('.player-board'));
-initializeGrid(document.querySelector('.opp-board'));
+flow.player2.playerBoard.placeShip(flow.player2.playerBoard.ships.battleship, [1, 1], false);
+flow.player2.playerBoard.placeShip(flow.player2.playerBoard.ships.carrier, [8, 4], false);
+flow.player2.playerBoard.placeShip(flow.player2.playerBoard.ships.destroyer, [2, 10], true);
+flow.player2.playerBoard.placeShip(flow.player2.playerBoard.ships.patrol, [9, 9], false);
+flow.player2.playerBoard.placeShip(flow.player2.playerBoard.ships.submarine, [3, 3], false);
+
+console.log(flow);
+
+display.initializeGrid(document.querySelector('.player-board'));
+display.initializeGrid(document.querySelector('.opp-board'));
+
+flow.player2.playerBoard.receiveAttack([1,1]);
+
+display.renderPlayerBoard(flow.player1);
+display.renderOpponentBoard(flow.player2);
+
+
